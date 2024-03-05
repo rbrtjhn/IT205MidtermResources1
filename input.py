ae2579a27653c9587de3d03e5564d21af424788f
Robert John Jotojot <153427974+rbrtjhn@users.noreply.github.com>
2024-03-05 15:44:10 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,10 @@
+#input() has string data type
+num1 = int(input("Enter 1st number: "))
+
+#int() for converting string to integer
+int(num1)
+num2 = int(input("Enter 2nd number: "))
+
+sum = num1 + num2
+
+print(f"The sum of {num1} and {num2} is: {sum}")
---
+++ output.py
@@ -0,0 +1,13 @@
+# Variable declaration
+x = 19
+message = "Hello."
+ratio = 3.14
+
+# Checking datatype with type() method
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+# String formatting
+print(message + " My name is Robert. I'm {} years old.".format(x))
+print(f"{message} My name is Robert. I am {x} years old.")
