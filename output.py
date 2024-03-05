c2974ea0d7127420415bac8db3fb53cf75e9c255
Robert John Jotojot <153427974+rbrtjhn@users.noreply.github.com>
2024-03-05 15:46:54 +0800

    output.py

--- output.py
+++ output.py
@@ -9,5 +9,5 @@ print(type(ratio))
 print(type(x))
 
 # String formatting
-print(message + " My name is Robert. I'm {} years old.".format(x))
+print(message + " My name is Robert. I'm " + str(x) + " years old.")
 print(f"{message} My name is Robert. I am {x} years old.")
